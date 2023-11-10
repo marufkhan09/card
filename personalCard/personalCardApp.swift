f5546af2ebfa2018aa62baaedca84bad5288f55b
marufkhan09 <maruf1609@gmail.com>
2023-11-11 04:14:31 +0600

    Initial Commit

---
+++ personalCard/personalCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  personalCardApp.swift
+//  personalCard
+//
+//  Created by Maruf Khan on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct personalCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
